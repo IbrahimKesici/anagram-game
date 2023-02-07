f665186679dd7ebdf0eef27e851a3b58b584217b
kesici <ibrahim.kesici@outlook.com>
2023-02-07 00:29:08 +0000

    initial commit

---
+++ src/main/java/com/java/anagramgame/AnagramGameApplication.java
@@ -0,0 +1,13 @@
+package com.java.anagramgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnagramGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnagramGameApplication.class, args);
+	}
+
+}
